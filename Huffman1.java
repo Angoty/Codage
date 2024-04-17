20c4435228c16bf1256774accd48d74adf163ed5
Angoty <118533095+Angoty@users.noreply.github.com>
2024-04-17 14:38:51 +0200

    third-commit

--- Huffman1.java
+++ Huffman1.java
@@ -46,16 +46,38 @@ public class Huffman1{
         return encodage.toString();
     }
 
+    // question 1 -c
+    public static String decoderTexte(String texteEncode, HashMap<String, Character> reverse){
+        StringBuilder decodage= new StringBuilder();
+        int voalohany= 0;
+        for(int b=1; b<=texteEncode.length(); b++){
+            String code = texteEncode.substring(voalohany, b);
+            if(reverse.containsKey(code)){
+                decodage.append(reverse.get(code));
+                voalohany = b;
+            }
+        }
+        return decodage.toString();
+    }
+
     public static void main(String[] args){
         char[] lettres= {'a', 'b', 'c', 'd', 'e', 'f'};
         double[] proba= {0.3, 0.2, 0.15, 0.15, 0.1, 0.1};
         HashMap<Character, String> code=new HashMap<>();
         construireArbre(lettres, proba, code);
+
+        HashMap<String, Character> reverse=new HashMap<>();
+        for(Map.Entry<Character, String> entry : code.entrySet()){
+            reverse.put(entry.getValue(), entry.getKey());
+        }
+
         System.out.println("Codes:"+ code);
         String text="abcd";
         String encode= encoderTexte(text, code);
+        String decode= decoderTexte(encode, reverse);
         System.out.println("Etat original: "+ text);
         System.out.println("Etat encode: "+encode);
+        System.out.println("Etat decode: "+decode);
 
     }
     
